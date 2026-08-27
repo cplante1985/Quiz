315e842939507fdafed3908208999161c0bf8bf6
Chyna Plante <chyna.plante@snhu.edu>
2026-08-27 16:01:38 -0400

    Add files via upload

---
+++ BTS_quiz.cpp
@@ -0,0 +1,520 @@
+/*Name:Chyna Plante
+August 24th, 2026
+*/
+
+#include  <iostream>
+
+int main() {
+
+    int song_1;
+    int song_2;
+    int song_3;
+    int song_4;
+    int song_5;
+    int song_6;
+    int song_7;
+    int song_8;
+    int song_9;
+    int song_10;
+    int song_11;
+    int song_12;
+    int song_13;
+    int song_14;
+    int song_15;
+    int song_16;
+  
+    int answer1;
+    int answer2;
+    int answer3;
+    int answer4;
+    int answer5;
+    int answer6;
+    int answer7;
+    int answer8;
+    int answer9;
+    int answer10;
+    int answer11;
+    int answer12;
+    int answer13;
+    int answer14;
+    int answer15;
+    int answer16;
+
+
+
+    int correctCount = 0;
+    int incorrectCount = 0;
+
+
+    std::cout << "=============================================\n";
+    std::cout << ("The BTS Quiz: Match the song with the lyrics\n");
+    std::cout << ("Can you remember these iconic lyrics from BTS\n");
+    std::cout << ("The quiz will be separated in three levels. From Easy to Hard\n");
+    std::cout << "=============================================\n";
+
+    std::cout << ("Level: Easy\n");
+
+
+    std::cout << ("Q1. Lookin' like a goody, goody in this bad world, bad world.Don't know how to act, girl\n");
+
+std::cout << "1. Swim\n";
+std::cout << "2. Don't say you love me\n";
+std::cout << "3. Standing Next to You\n";
+std::cout << "4. Seven\n";
+
+std::cin >> answer1;
+
+if (answer1 == 1 && song_1 == 1) {
+    std::cout << ("Correct");
+    correctCount++;
+}
+else if (answer2 == 2 && song_1 == 2) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_1 == 3) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_1 == 4) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input. Try again\n");
+}
+
+std::cout << ("Q2. Did you see my bag? (Where?) Did you see my bag? (Where?).My bag's filled with trophies\n");
+
+std::cout << "1. Stay Gold\n";
+std::cout << "2. Mic Drop\n";
+std::cout << "3. Dope\n";
+std::cout << "4. 2.0\n";
+
+std::cin >> answer2;
+
+if (answer1 == 1 && song_2 == 1) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_2 == 2) {
+    std::cout << ("Correct");
+    correctCount++;
+}
+else if (answer3 == 3 && song_3 == 3) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_4 == 4) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input. Try again\n");
+}
+
+std::cout << ("Q3.But I don't wanna use my head, I don't wanna calculate.Love ain't a business, rather like a fitness\n");
+
+std::cout << "1. Run\n";
+std::cout << "2. Come Over\n";
+std::cout << "3. Danger\n";
+std::cout << "4. Love Maze\n";
+
+std::cin >> answer3;
+
+if (answer1 == 1 && song_3 == 1) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_3 == 2) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_3 == 3 ) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_3 == 4) {
+    std::cout << ("Correct");
+    correctCount++;
+}
+else {
+    std::cout << ("Invalid input. Try again\n");
+}
+
+std::cout << ("Q4.Run away, out of sight. don't know what I want.Wish I had a minute just to turn me off\n");
+
+std::cout << "1. Normal\n";
+std::cout << "2. Alien\n";
+std::cout << "3. Make It Right\n";
+std::cout << "4. My Universe\n";
+
+std::cin >> answer4;
+
+if (answer1 == 1 && song_4 == 1) {
+    std::cout << ("Correct");
+    correctCount++;
+}
+else if (answer2 == 2 && song_4 == 2) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_4 == 3) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_4 == 4) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input. Try again\n");
+}
+
+std::cout << ("Q5 Take my hands now.You are the cause of my euphoria\n");
+
+std::cout << "1. Haegeum\n";
+std::cout << "2. Euphoria\n";
+std::cout << "3. Life Goes On\n";
+std::cout << "4. Boy With Luv\n";
+
+std::cin >> answer5;
+
+if (answer1 == 1 && song_5 == 1) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_5 == 2) {
+    std::cout << ("Correct");
+    correctCount++;
+}
+else if (answer3 == 3 && song_5 == 3) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_5 == 4) {
+    std::cout << ("Incorrect");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input. Try again\n");
+}
+
+std::cout << ("Level: Medium\n");
+
+std::cout << ("Q6.(Stay gold) Yume no naka demo.(Stay gold) Sagashiateru yo\n");
+
+std::cout << "1. Heartbeat\n";
+std::cout << "2. Film Out\n";
+std::cout << "3. Stay Gold\n";
+std::cout << "4. My Universe\n"
+
+std::cin >> answer6;
+
+if (answer1 == 1 && song_6 == 1) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_6 == 2 ) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_6 == 3) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else if (answer4 == 4 && song_6 == 4) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input\n");
+}
+
+std::cout << ("Q7.Nae ane gatyeoseo nan jugeo isseo (nan jugeoisseo)Don't wanna be lonely, just wanna be yours\n");
+
+std::cout << "1. I Need U\n";
+std::cout << "2. My You\n";
+std::cout << "3. Butterfly\n";
+std::cout << "4. Save Me\n";
+
+std::cin >> answer7;
+
+if (answer1 == 1 && song_7 == 1) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_7 == 2 ) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_7 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_7 == 4) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else {
+    std::cout << ("Invalid input\n");
+}
+
+std::cout << ("Q8. Woah-oh-oh, yeah.I don't know your name\n");
+
+std::cout << "1. Amygdala\n";
+std::cout << "2. Film Out\n";
+std::cout << "3. Tear\n";
+std::cout << "4. Fire\n;"
+
+std::cin >> answer8;
+
+if (answer1 == 1 && song_8 == 1) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else if (answer2 == 2 && song_8 == 2 ) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_8 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_8 == 4) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input\n");
+}
+
+std::cout << ("Q9.He too was a tempter; he, too, was a link to the second, the evil world with which I no longer wanted to have anything to do\n");
+
+std::cout << "1. Make It Right\n";
+std::cout << "2. Alien\n";
+std::cout << "3. Merry Go Round\n";
+std::cout << "4. Blood Sweat & Tears\n";
+
+std::cin >> answer9;
+
+if(answer1 == 1 && song_9 == 1) {
+   std::cout << ("Incorrect\n");
+   incorrectCount++;
+}
+else if (answer2 == 2 && song_9 == 2) {
+     std::cout <<("Incorrect\n");
+     incorrectCount++;
+}
+else if (answer3 == 3 && song_9 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_9 == 4) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else {
+    std::cout << ("Invalid input\n");
+}
+
+std::cout << ("Q10.Beol banneun geon anijana, iri wa nan neoui paradise (Paradise).Can't close your eyes (Can't close your eyes\n");
+
+std::cout << "1. Into the Sun\n";
+std::cout << "2. Pied Piper\n";
+std::cout << "3. Permission to Dance\n";
+std::cout << "4. Crystal Snow\n";
+
+std::cin >> answer10;
+
+if (answer1 == 1 && song_10 == 1) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_10 == 2) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else if (answer3 == 3 && song_10 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_10 == 4) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input\n");
+}
+
+std::cout << ("Level:Hard\n");
+
+std::cout << ("Q11.Omotta yori sekai wa hayaku te.How we gonna change it? We don't know yet.Demo kitto?\n"); 
+
+std::cout << "1. Dynamite\n";
+std::cout << "2. CrystalSnow\n";
+std::cout << "3. Friends\n";
+std::cout << "4. Super Tuna\n";
+
+std::cin >> answer11;
+
+if (answer1 == 1 && song_11 == 1) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_11 == 2) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else if (answer3 == 3 && song_11 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_11 == 4) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input\n");
+}
+
+std::cout << ("Q12.Tell me, am I ever gonna feel again? Tell me, am I ever gonna heal again?\n");
+
+std::cout << ("1. Too Sad to Dance\n");
+std::cout << ("2. Shot Glass of Tears\n");
+std::cout << ("3. Butter\n");
+std::cout << ("4. Magic Shop\n");
+
+std::cin >> answer12;
+
+if (answer1 == 1 && song_12 == 1) {
+   std::cout << ("Incorrect\n");
+   incorrectCount++;
+}
+else if (answer2 == 2 && song_12 == 2) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else if (answer3 == 3 && song_12 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_12 == 4) {
+    std::cout <<("Incorrect\n");
+    incorrectCount++;
+}
+else {
+    std::cout << ("Invalid input\n");
+}
+
+std::cout << ("Q13.Sorichyeobwa alright (Alright)Momi tabeoridorok all night (All night).Cause we got fire (Fire\n");
+
+std::cout << "1. Fire\n";
+std::cout << "2. Closer Than This\n";
+std::cout << "3. Dope\n";
+std::cout << "4. Black Swan\n";
+
+std::cin >> answer13;
+
+if (answer1 == 1 && song_13 == 1) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_13 == 2) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_13 == 3) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else if (answer4 == 4 && song_13 == 4) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+
+std::cout << ("Q14.Kimi dake wo hiroi atsumete tsunagete.Heya juu ni utsushite nagame nagara\n");
+
+std::cout << "1. Never Let Go\n";
+std::cout << "2. Film Out\n";
+std::cout << "3. Killin it girl\n";
+std::cout << "4. Standing next to you\n";
+
+std::cin >> answer14;
+
+if (answer1 == 1 && song_14 == 1) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_14 == 2) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+else if (answer3 == 3 && song_14 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_14 == 4) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+
+std::cout << ("Q15.Talkin', talkin', talkin'.I do what I do, geunikka neon neona jalhasyeo\n");
+
+std::cout << "1. DNA\n";
+std::cout << "2. Alien\n";
+std::cout << "3. Not Today\n";
+std::cout << "4. IDOL\n";
+
+std::cin >> answer15;
+
+if (answer1 == 1 && song_15 == 1) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_15 == 2) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_15 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_15 == 4) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+
+std::cout << ("Q16 You know I get too caught up in a moment,I can't call it love if I show it. I just fuck things up if you noticed.\n");
+
+std::cout << "1. First Love\n";
+std::cout << "2. Outro: Her\n";
+std::cout << "3. Lilith\n";
+std::cout << "4. Normal\n";
+
+std::cin >> answer16;
+
+if (answer1 == 1 && song_16 == 1) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer2 == 2 && song_16 == 2) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer3 == 3 && song_16 == 3) {
+    std::cout << ("Incorrect\n");
+    incorrectCount++;
+}
+else if (answer4 == 4 && song_16 == 4) {
+    std::cout << ("Correct\n");
+    correctCount++;
+}
+
+std::cout << ("\n----Results-------\n");
+std::cout <<"Correct answers: " << correctCount << std::endl;
+
+std::cout << "Incorrect answers : " << incorrectCount << std::endl;
+
+return 0;
+}
